6eb9a5e3e416498b4318c051f863df94f9285ab6
jaber <j.donyavi@hotmail.com>
2015-01-10 12:41:48 +0000

    user view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   
-  get 'users' => 'users#new'
+  resources :users
   
   get 'home' => 'static_pages#home'
   get 'help' => 'static_pages#help'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,13 @@
 
 ActiveRecord::Schema.define(version: 20150108164058) do
 
+  create_table "infos", force: true do |t|
+    t.string   "title"
+    t.string   "desc"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
